3385c03d74a7e7a55409870b5a7c6ad062b11689
Frederik deHaas <dehaas@thylacine.zoology.ubc.ca>
2019-01-16 12:07:23 -0800

    working mathematica+Cpp integration

--- Cppfiles/main.cpp
+++ Cppfiles/main.cpp
@@ -3,35 +3,103 @@
 #include <vector>
 #include <fstream>
 #include <cassert>
-//#include "WolframLibrary.h"
-#include <iostream>
+#include "WolframLibrary.h"
 
-//EXTERN_C DLLEXPORT mint WolframLibrary_getVersion(){return WolframLibraryVersion;}
-//EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) {return 0;}
-//EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) {}
+EXTERN_C DLLEXPORT mint WolframLibrary_getVersion(){return WolframLibraryVersion;}
+EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) {return 0;}
+EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData) {}
 
 //include openmp
-
 enum cases {birth1, birth2, death1, death2};
 
 struct parameters{
     public:
-    double b1, b2, d1, d2, k1, k2;
-    int tmax;
-    int nrep;
+    parameters(MArgument *Args):
+        b1(MArgument_getReal(Args[2])),
+        b2(MArgument_getReal(Args[3])),
+        d1(MArgument_getReal(Args[4])),
+        d2(MArgument_getReal(Args[5])),
+        k1(MArgument_getInteger(Args[6])),
+        k2(MArgument_getInteger(Args[7])),
+        tmax(MArgument_getInteger(Args[8])),
+        nrep(MArgument_getInteger(Args[9])){}
+    const double b1, b2, d1, d2, k1, k2;
+    const int tmax;
+    const int nrep;
+};
+
+struct moments{
+    public:
+    moments():firstM(2,0), secondM(3,0), thirdM(4,0), firstM_moment(2,0.0), secondM_Cmoment(3,0.0), thirdM_Cmoment(5,0.0)
+    {
+    };
+
+    void update(const int &n1, const int &n2){
+        firstM[0] += n1;
+        firstM[1] += n2;
+        secondM[0] += n1*n1;
+        secondM[1] += n1*n2;
+        secondM[2] += n2*n2;
+        thirdM[0] += n1*n1*n1;
+        thirdM[1] += n1*n1*n2;
+        thirdM[2] += n1*n2*n2;
+        thirdM[3] += n2*n2*n2;
+        ++counter;
+    }
+
+    void calcCentralMoments(){
+        std::vector<double> secondM_moment(3,0.0);
+        std::vector<double> thirdM_moment(4,0.0);
+
+        firstM_moment[0] = (long double)firstM[0]/(long double)counter;
+        firstM_moment[1] = (long double)firstM[1]/(long double)counter;
+        secondM_moment[0] = (long double)secondM[0]/(long double)counter;
+        secondM_moment[1] = (long double)secondM[1]/(long double)counter;
+        secondM_moment[2] = (long double)secondM[2]/(long double)counter;
+        thirdM_moment[0] = (long double)thirdM[0]/(long double)counter;
+        thirdM_moment[1] = (long double)thirdM[1]/(long double)counter;
+        thirdM_moment[2] = (long double)thirdM[2]/(long double)counter;
+        thirdM_moment[3] = (long double)thirdM[3]/(long double)counter;
+
+        secondM_Cmoment[0] = secondM_moment[0] - firstM_moment[0]*firstM_moment[0];
+        secondM_Cmoment[1] = secondM_moment[1] - firstM_moment[0]*firstM_moment[1];
+        secondM_Cmoment[2] = secondM_moment[2] - firstM_moment[1]*firstM_moment[1];
+        thirdM_Cmoment[0] = thirdM_moment[0] - firstM_moment[0]*firstM_moment[0]*firstM_moment[0];
+        thirdM_Cmoment[1] = thirdM_moment[1] - secondM_moment[0]*firstM_moment[1]+2*firstM_moment[0]*firstM_moment[0]*firstM_moment[1]-2*firstM_moment[0]*secondM_moment[1];
+        thirdM_Cmoment[2] = thirdM_moment[2] - secondM_moment[2]*firstM_moment[0]+2*firstM_moment[0]*firstM_moment[1]*firstM_moment[1]-2*firstM_moment[1]*secondM_moment[1];
+        thirdM_Cmoment[3] = thirdM_moment[3] - firstM_moment[1]*firstM_moment[1]*firstM_moment[1];
+    }
+
+    double returnmean(const int &i){
+        return firstM_moment[i];
+    }
+    double retursecondCM(const int &i){
+        return secondM_Cmoment[i];
+    }
+    double returnthirdCM(const int &i){
+        return thirdM_Cmoment[i];
+    }
+
+    private:
+    int counter = 0;
+    std::vector<long int> firstM;
+    std::vector<long int> secondM;
+    std::vector<long int> thirdM;
+    std::vector<double> firstM_moment;
+    std::vector<double> secondM_Cmoment;
+    std::vector<double> thirdM_Cmoment;
 };
 
 class Gillespie{
     public:
-    Gillespie(const int &n1, const int &n2, const parameters &pars, int** counts_type1, int** counts_type2):
-        pars(pars), counts_type1(counts_type1), counts_type2(counts_type2), n1(n1),n2(n2){
+    Gillespie(const int &n1, const int &n2, const parameters &pars): pars(pars), n1(n1),n2(n2){
     t=0.0; 
     twrite = 0; 
     }
 
-    bool MCstep(){
-        assert(n1+n2>0);
-        assert(pars.k2>0); assert(pars.k1>0);
+    bool MCstep(std::vector<std::vector<int> > &data1, std::vector<std::vector<int> > &data2){
+        //assert(n1+n2>0);
+        //assert(pars.k2>0); assert(pars.k1>0);
         rnd::discrete_distribution dist(4);
         dist[birth1] = (n1>0 && n1+n2-1<pars.k2) ? pars.b1*(double)n1*(1.0-((double)(n1+n2))/pars.k2) : 0.0;
         dist[birth2] = (n2>0 && n1+n2-1<pars.k1) ? pars.b2*(double)n2*(1.0-((double)(n1+n2))/pars.k1) : 0.0;
@@ -41,7 +109,7 @@ class Gillespie{
         tprime =t+ rnd::exponential(1.0/(dist[death1]+dist[death2]+dist[birth1]+dist[birth2]));
         
         if(tprime >= pars.tmax){
-            writedata(n1,n2,pars.tmax);
+            writedata(n1,n2,data1, data2, pars.tmax);
             return false;
         } 
         else{
@@ -53,12 +121,12 @@ class Gillespie{
             }
 
             if(n1+n2==0){               
-                writedata(n1,n2,pars.tmax);
+                writedata(n1,n2,data1, data2, pars.tmax);
                 return false;
             }
 
             if(std::floor(t) < std::floor(tprime)){
-                writedata(n1,n2,(int)std::floor(tprime));
+                writedata(n1,n2,data1, data2, (int)std::floor(tprime));
             }
         }
         t = tprime;
@@ -74,167 +142,162 @@ class Gillespie{
     const parameters pars;
     int twrite;
 
-    void writedata(const int &n1, const int &n2, const int &tmax){
+    void writedata(const int &n1, const int &n2,std::vector<std::vector<int> > &data1, std::vector<std::vector<int> > &data2, const int &tmax){
         for(;twrite < tmax; ++twrite){
-            ++counts_type1[twrite][n1];
-            ++counts_type2[twrite][n2];
+            ++data1[twrite][n1];
+            ++data2[twrite][n2];
         }
     }
 
-    int** counts_type1;
-    int** counts_type2;
+    // int** counts_type1;
+    // int** counts_type2;
 };
 
-int main(){
-    rnd::set_seed();
-    const int n1_init = 1;
-    const int n2_init = 1;
-    parameters pars;
-    pars.b1 = 0.2;
-    pars.b2 = 0.2;
-    pars.d1 = 0.05;
-    pars.d2 = 0.05;
-    pars.k1 = 2;
-    pars.k2 = 2;
-    pars.tmax = 100;
-    pars.nrep = 100;
-    assert(n1_init <= pars.k1);
-    assert(n2_init <= pars.k2);
-    
-    const int rowCount = pars.tmax;
-    const int colCount1 = pars.k1+1;
-    const int colCount2 = pars.k2+1;
-    int** counts_type1 = new int*[rowCount];
-    int** counts_type2 = new int*[rowCount];
-    for(int i = 0; i < rowCount; ++i){
-        counts_type1[i] = new int[colCount1];
-        counts_type2[i] = new int[colCount2];
+class GillespieMoments{
+    public:
+    GillespieMoments(const int &n1, const int &n2, const parameters &pars): pars(pars), n1(n1),n2(n2){
+    t=0.0; 
+    twrite = 0; 
     }
 
-    for(int i = 0; i < colCount1; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            counts_type1[j][i] =0;
+    bool MCstep(std::vector<moments> &M){
+        //assert(n1+n2>0);
+        //assert(pars.k2>0); assert(pars.k1>0);
+        rnd::discrete_distribution dist(4);
+        dist[birth1] = (n1>0 && n1+n2-1<pars.k2) ? pars.b1*(double)n1*(1.0-((double)(n1+n2))/pars.k2) : 0.0;
+        dist[birth2] = (n2>0 && n1+n2-1<pars.k1) ? pars.b2*(double)n2*(1.0-((double)(n1+n2))/pars.k1) : 0.0;
+        dist[death1] = pars.d1*(double)n1;
+        dist[death2] = pars.d2*(double)n2;
+
+        tprime =t+ rnd::exponential(1.0/(dist[death1]+dist[death2]+dist[birth1]+dist[birth2]));
+        
+        if(tprime >= pars.tmax){
+            writedata(n1,n2, M, pars.tmax);
+            return false;
+        } 
+        else{
+            switch(dist.sample()) {
+                case birth1 : n1+=1; break;
+                case birth2 : n2+=1; break;
+                case death1 : n1-=1; break;
+                case death2 : n2-=1; break;
+            }
+
+            if(n1+n2==0){               
+                writedata(n1,n2, M, pars.tmax);
+                return false;
+            }
+
+            if(std::floor(t) < std::floor(tprime)){
+                writedata(n1,n2, M, (int)std::floor(tprime));
+            }
+        }
+        t = tprime;
+        
+        return true;
+    }
+    private:
+    double t;
+    double tprime;
+    int n1;
+    int n2;
+    const parameters pars;
+    int twrite;
+
+    void writedata(const int &n1, const int &n2, std::vector<moments> &M , const int &tmax){
+        for(;twrite < tmax; ++twrite){
+            M[twrite].update(n1,n2);
         }
     }
-    for(int i = 0; i < colCount2; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            counts_type2[j][i] =0;
-        }
-    }
+};
 
-    // Run simulations (parallel)?
-    #pragma omp parallel
-    #pragma omp for
-    for(int i = 0; i < pars.nrep; ++i){
-        Gillespie test(n1_init,n2_init,pars, counts_type1, counts_type2);  
-        while(test.MCstep());
-    }
-    
-    std::ofstream outputcsv1("data_type1.csv");
-    outputcsv1.fill(',');
-    std::ofstream outputcsv2("data_type2.csv");
-    outputcsv2.fill(',');
-
-    for(int i = 0; i < colCount1; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            outputcsv1 << counts_type1[j][i] << outputcsv1.fill();
-        }
-
-    outputcsv1 << std::endl;
-    }
-
-    for(int i = 0; i < colCount2; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            outputcsv2 << counts_type2[j][i] << outputcsv2.fill();
-        }
-    outputcsv2 << std::endl;
-    }
-
-    return 0;
-}
-
-/*
-EXTERN_C DLLEXPORT int myFunction2(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res){
-    
-    int out = MArgument_getInteger(Args[0]) + MArgument_getInteger(Args[1]);
-    MArgument_setInteger(Res,out);
-    return LIBRARY_NO_ERROR;
+bool verifypars(const int &n1_init, const int &n2_init, const parameters &pars, WolframLibraryData &libData){
+    if(pars.k1 < n1_init){
+        libData->Message("k1<n1");
+        return true;
+    };
+    if(pars.k2 < n2_init){
+        libData->Message("k2<n2");
+        return true;
+    };
+    if(pars.b1 <= 0.0){
+        libData->Message("b1<=0");
+        return true;
+    };
+    if(pars.b2 <= 0.0){
+        libData->Message("b2<=0");
+        return true;
+    };
+    if(pars.d1 <= 0.0){
+        libData->Message("d1<=0");
+        return true;
+    };
+    if(pars.d2 <= 0.0){
+        libData->Message("d2<=0");
+        return true;
+    };
+    if(pars.tmax < 1){
+        libData->Message("tmax_lowerbound");
+        return true;
+    };
+    return false; 
 }
 
 EXTERN_C DLLEXPORT int myFunction(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res){
-
     rnd::set_seed();
+    parameters pars(Args);
+
     const int n1_init = MArgument_getInteger(Args[0]);
-    const int n2_init = MArgument_getInteger(Args[0]);
-    parameters pars;
-    pars.b1 = 0.2;
-    pars.b2 = 0.6;
-    pars.d1 = 0.05;
-    pars.d2 = 0.05;
-    pars.k1 = 10;
-    pars.k2 = 10.0/3.0;
-    pars.tmax = 100;
-    pars.nrep = 100;
-    assert(n1_init <= pars.k1);
-    assert(n2_init <= pars.k2);
-    
-    const int rowCount = pars.tmax;
-    const int colCount1 = pars.k1;
-    const int colCount2 = pars.k2;
-    int** counts_type1 = new int*[rowCount];
-    int** counts_type2 = new int*[rowCount];
-    for(int i = 0; i < rowCount; ++i){
-        counts_type1[i] = new int[colCount1];
-        counts_type2[i] = new int[colCount2];
-    }
+    const int n2_init = MArgument_getInteger(Args[1]);
 
-    for(int i = 0; i < pars.k1; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            counts_type1[j][i] =0;
-        }
-    }
-    for(int i = 0; i < pars.k2; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            counts_type2[j][i] =0;
-        }
-    }
+    if(verifypars(n1_init,n2_init, pars, libData)){
+        return LIBRARY_FUNCTION_ERROR;
+    };
 
-    // Run simulations (parallel)?
+    std::vector<moments> M(pars.tmax);
     for(int i = 0; i < pars.nrep; ++i){
-        Gillespie test(n1_init,n2_init,pars, counts_type1, counts_type2);  
-        while(test.MCstep());
+        GillespieMoments test(n1_init,n2_init, pars); 
+        while(test.MCstep(M));
     }
 
+    //Create output for mathematica
+    MTensor T0;
+    mint type = MType_Real;
+    mint dims[2];
+    mint rank = 2;
+    int err;
+    dims[0] = pars.tmax;
+    dims[1] = 10;
+    err = libData->MTensor_new( type, rank, dims, &T0);
 
-    MArgument_setInteger(Res,100);
+    for ( mint i = 1; i <= pars.tmax && !err; i++) {
+        M[i-1].calcCentralMoments();
+        mint lens[2];
+        lens[0] = i;
 
- return LIBRARY_NO_ERROR;
+        lens[1] = 1;
+        err = libData->MTensor_setReal( T0, lens, (double)i);
+        lens[1] = 2;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].returnmean(0));
+        lens[1] = 3;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].returnmean(1));
+        lens[1] = 4;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].retursecondCM(0));
+        lens[1] = 5;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].retursecondCM(1));
+        lens[1] = 6;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].retursecondCM(2));
+        lens[1] = 7;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].returnthirdCM(0));
+        lens[1] = 8;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].returnthirdCM(1));
+        lens[1] = 9;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].returnthirdCM(2));
+        lens[1] = 10;
+        err = libData->MTensor_setReal( T0, lens, M[i-1].returnthirdCM(3));
+    }
 
+    MArgument_setMTensor(Res, T0);
+
+    return LIBRARY_NO_ERROR; 
 }
-
-
- Graveyard 
-
-
-    for(int j = 0; j < pars.tmax; ++j){
-        double avg = 0.0; 
-        for(int i = 0; i < pars.k1; ++i){
-            avg += i*counts_type1[j][i];
-            outputcsv1 << (double)counts_type1[j][i]/(double)pars.nrep << outputcsv1.fill();
-        }
-
-    outputcsv1 << std::endl;
-    }
-
-    std::cout << "\n \n";
-
-    for(int i = 0; i < pars.k2; ++i){
-        for(int j = 0; j < pars.tmax; ++j){
-            outputcsv2 << (double)counts_type2[j][i]/(double)pars.nrep << outputcsv2.fill();
-        }
-    outputcsv2 << std::endl;
-    }
-
-    return 0;
-*/
-
